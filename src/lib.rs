d5a2ae7edf8838f1fb2cf802d92ef639e0f8b485
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-10-23 17:42:34 +0200

    Support JWT token for GET requests

---
+++ src/lib.rs
@@ -0,0 +1,525 @@
+#[derive(Debug, serde::Serialize)]
+pub struct CallResult {
+    payload: String,
+    path: String,
+    status_code: u16,
+}
+
+pub async fn do_it(
+    openapi_schema: openapiv3::OpenAPI,
+    url: Option<String>,
+    jwt: Option<String>,
+) -> Result<Vec<Vec<CallResult>>, reqwest::Error> {
+    tracing::info!("openapi version: {}", openapi_schema.openapi);
+
+    let components = match openapi_schema.components {
+        Some(c) => c,
+        None => {
+            tracing::error!("No components found in the openapi schema");
+            std::process::exit(1);
+        }
+    };
+
+    let base_url = match openapi_schema.servers.first() {
+        Some(s) => s.url.clone(),
+        None => {
+            tracing::error!("No servers found in the openapi schema");
+            std::process::exit(1);
+        }
+    };
+
+    let base_url = match url {
+        Some(b) => b,
+        None => base_url,
+    };
+
+    let jwt_name = get_jwt_token(&components);
+
+    let mut posts = collect_post(&openapi_schema.paths);
+    populate_payload(&mut posts, components);
+
+    let gets = collect_gets(&openapi_schema.paths);
+
+    let mut operations = vec![];
+    operations.extend_from_slice(gets.as_slice());
+    operations.extend_from_slice(posts.as_slice());
+
+    let mut all_results = vec![];
+
+    for p in operations {
+        let result = exec_operation(p.clone(), &base_url, (jwt_name.clone(), jwt.clone())).await;
+        match result {
+            Ok(r) => all_results.push(r),
+            Err(e) => {
+                tracing::error!("Error executing operation: {:?}", e);
+                return Err(e);
+            }
+        }
+    }
+
+    Ok(all_results)
+}
+
+fn get_jwt_token(components: &openapiv3::Components) -> Option<String> {
+    components
+        .security_schemes
+        .iter()
+        .filter_map(|(k, v)| {
+            let v = v.as_item();
+            v.map(|scheme| (k.clone(), scheme.clone()))
+        })
+        .find_map(|(k, v)| match v {
+            openapiv3::SecurityScheme::HTTP {
+                scheme, // FIXME: do we need to validate other fields here?
+                ..
+            } if scheme.to_lowercase() == "bearer" => Some(k),
+            _ => None,
+        })
+}
+
+fn collect_gets(paths: &openapiv3::Paths) -> Vec<Op> {
+    paths
+        .iter()
+        .map(|p| {
+            let pp = p.0.to_owned();
+            let i = p.1.to_owned();
+            let i = i.as_item();
+            let i = i.unwrap();
+            (pp, i.clone())
+        })
+        .filter(|p| p.1.get.is_some())
+        .filter(|p| !p.1.get.as_ref().unwrap().deprecated)
+        .map(|p| Op {
+            path: p.0,
+            method: "GET".to_owned(),
+            operation: p.1.get.unwrap(),
+            payload: None,
+        })
+        .collect()
+}
+
+fn collect_post(paths: &openapiv3::Paths) -> Vec<Op> {
+    paths
+        .iter()
+        .map(|p| {
+            let pp = p.0.to_owned();
+            let i = p.1.to_owned();
+            let i = i.as_item();
+            let i = i.unwrap();
+            (pp, i.clone())
+        })
+        .filter(|p| p.1.post.is_some())
+        .filter(|p| !p.1.post.as_ref().unwrap().deprecated)
+        .map(|p| {
+            let post = p.1.post.unwrap();
+            let path = p.0.to_owned();
+            (path, post)
+        })
+        .filter(|p| p.1.request_body.is_some())
+        .filter(|p| {
+            let req_body = p.1.request_body.as_ref().unwrap();
+            let req_body = req_body.as_item().unwrap();
+            req_body
+                .content
+                .iter()
+                .any(|(k, _)| k == "application/json")
+        })
+        .map(|p| Op {
+            path: p.0,
+            method: "POST".to_owned(),
+            operation: p.1,
+            payload: None,
+        })
+        .collect()
+}
+
+#[derive(Clone)]
+struct Op {
+    path: String,
+    method: String,
+    operation: openapiv3::Operation,
+    payload: Option<openapiv3::Schema>,
+}
+
+fn populate_payload(op: &mut Vec<Op>, components: openapiv3::Components) {
+    for o in op {
+        let req = match &o.operation.request_body {
+            Some(r) => r,
+            None => continue,
+        };
+
+        let req = match req.as_item() {
+            Some(r) => r,
+            None => continue,
+        };
+
+        for (_, media_type) in &req.content {
+            let schema = match &media_type.schema {
+                Some(s) => s,
+                None => continue,
+            };
+
+            let reference = match schema {
+                openapiv3::ReferenceOr::Reference { reference } => reference.clone(),
+                openapiv3::ReferenceOr::Item(_) => "".to_owned(),
+            };
+
+            if reference.is_empty() {
+                tracing::warn!("reference is empty for request {}", o.path);
+                continue;
+            }
+
+            let reference = reference.trim_start_matches("#/components/schemas/");
+            let schema = match components.schemas.get(reference) {
+                Some(s) => s,
+                None => continue,
+            };
+
+            let ss = schema.as_item();
+            o.payload = ss.cloned();
+        }
+    }
+}
+
+#[derive(Debug)]
+struct PropertyField {
+    example: Option<serde_json::Value>,
+    nullable: bool, // TODO: handle combinations with nullable fields
+}
+
+fn property_for_schema(s: &openapiv3::Schema) -> std::collections::HashMap<String, PropertyField> {
+    let mut properties = std::collections::HashMap::new();
+
+    match &s.schema_data.example {
+        Some(e) => {
+            for (k, v) in e.as_object().unwrap() {
+                let pf = PropertyField {
+                    example: Some(v.clone()),
+                    nullable: false,
+                };
+
+                properties.insert(k.to_owned(), pf);
+            }
+
+            return properties;
+        }
+        None => {
+            tracing::warn!(
+                "No paylaod object example found in the schema, using example of single property"
+            );
+        }
+    }
+
+    if let openapiv3::SchemaKind::Type(t) = &s.schema_kind {
+        match t {
+            openapiv3::Type::String(s) => {
+                println!("string: {:?}", s);
+            }
+            openapiv3::Type::Number(n) => {
+                println!("number: {:?}", n);
+            }
+            openapiv3::Type::Object(o) => {
+                for (k, v) in &o.properties {
+                    let v = v.as_item();
+                    let v = v.unwrap();
+                    let pf = PropertyField {
+                        example: v.schema_data.example.clone(),
+                        nullable: v.schema_data.nullable,
+                    };
+
+                    properties.insert(k.to_owned(), pf);
+                }
+            }
+            openapiv3::Type::Array(a) => {
+                println!("array: {:?}", a);
+            }
+            openapiv3::Type::Boolean(b) => {
+                println!("boolean: {:?}", b);
+            }
+            openapiv3::Type::Integer(i) => {
+                println!("integer: {:?}", i);
+            }
+        }
+    }
+
+    properties
+}
+
+async fn exec_operation(
+    op: Op,
+    base_url: &str,
+    (jwt_name, jwt): (Option<String>, Option<String>),
+) -> Result<Vec<CallResult>, reqwest::Error> {
+    match op.method.as_str() {
+        "GET" => {
+            drill_get_endpoint(base_url, &op.path, (jwt_name, jwt), op.operation.security).await
+        }
+        "POST" => {
+            let s = match op.payload {
+                Some(s) => s,
+                None => {
+                    tracing::warn!("No payload found for POST {}", op.path);
+                    return Ok(vec![]);
+                }
+            };
+            let mut props = property_for_schema(&s);
+            let combs = create_combination_property(&mut props);
+            drill_post_endpoint(base_url, &op.path, combs).await
+        }
+        _ => {
+            tracing::warn!("Unsupported method: {}", op.method);
+            Ok(vec![])
+        }
+    }
+}
+
+fn create_combination_property(
+    properties: &mut std::collections::HashMap<String, PropertyField>,
+) -> Vec<Vec<(&String, &PropertyField)>> {
+    let total_combinations = (1 << properties.len()) - 1;
+    let mut combination = vec![];
+
+    for mask in 1..=total_combinations {
+        let mut comb = vec![];
+
+        for (i, (name, value)) in properties.iter().enumerate() {
+            if (mask & (1 << i)) == 0 {
+                continue;
+            }
+
+            comb.push((name, value));
+        }
+
+        combination.push(comb);
+    }
+
+    combination
+}
+
+async fn drill_get_endpoint(
+    base_url: &str,
+    path: &str,
+    (jwt_name, jwt): (Option<String>, Option<String>),
+    security: Option<Vec<openapiv3::SecurityRequirement>>,
+) -> Result<Vec<CallResult>, reqwest::Error> {
+    let url = format!("{base_url}{path}");
+
+    let client = reqwest::Client::new();
+    let mut req = client.request(reqwest::Method::GET, url.clone());
+
+    if let Some(s) = security {
+        if jwt.is_some() && jwt_name.is_some() {
+            for ss in s.iter() {
+                for (k, _) in ss.iter() {
+                    let jwt_name = jwt_name.clone().unwrap();
+                    let jwt = jwt.clone().unwrap();
+
+                    if k == &jwt_name {
+                        req = req.header("Authorization", format!("Bearer {}", jwt));
+                    }
+                }
+            }
+        }
+    }
+
+    let r = req.build().unwrap(); // TODO: handle the error
+    let resp = client.execute(r).await?;
+
+    Ok(vec![CallResult {
+        payload: "".to_owned(),
+        path: url.to_string(),
+        status_code: resp.status().as_u16(),
+    }])
+}
+
+async fn drill_post_endpoint(
+    base_url: &str,
+    path: &str,
+    prop_combinations: Vec<Vec<(&String, &PropertyField)>>,
+) -> Result<Vec<CallResult>, reqwest::Error> {
+    let url = format!("{base_url}{path}");
+
+    let client = reqwest::Client::new();
+
+    let mut responses = vec![];
+
+    for properties in prop_combinations {
+        let mut paylaod = std::collections::HashMap::new();
+        for props in properties {
+            let pf = props.1;
+            paylaod.insert(
+                props.0,
+                pf.example.clone().unwrap_or(serde_json::Value::Null),
+            );
+        }
+
+        let s = serde_json::to_string(&paylaod).unwrap(); // TODO: handle the error
+
+        let req = client
+            .request(reqwest::Method::POST, url.clone()) // TODO: Make method configurable
+            .body(s.clone())
+            .header("Content-Type", "application/json"); // TODO: Make this configurable
+        let r = req.build().unwrap(); // TODO: handle the error
+        let resp = client.execute(r).await?;
+
+        responses.push(CallResult {
+            payload: s,
+            path: url.to_string(),
+            status_code: resp.status().as_u16(),
+        });
+    }
+
+    Ok(responses)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// This is a fake test to make sure the test suite is setup with tracing.
+    #[test]
+    fn fake_test() {
+        tracing_subscriber::fmt::init();
+        assert!(true);
+    }
+
+    #[test]
+    fn scan_get() {
+        let s = std::include_str!("./testdata/get_info.yml");
+        let openapi_schema = serde_yaml::from_str(s);
+        assert!(openapi_schema.is_ok());
+
+        let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+        let gets = collect_gets(&openapi_schema.paths);
+        assert_eq!(gets.len(), 1);
+    }
+
+    #[test]
+    fn scan_post() {
+        let s = std::include_str!("./testdata/post_login.yml");
+        let openapi_schema = serde_yaml::from_str(s);
+        assert!(openapi_schema.is_ok());
+
+        let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+        let posts = collect_post(&openapi_schema.paths);
+        assert_eq!(posts.len(), 1);
+        assert_eq!(posts.first().unwrap().path, "/api/v1/login");
+    }
+
+    #[test]
+    fn check_post_payload() {
+        let s = std::include_str!("./testdata/post_login.yml");
+        let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+        let mut posts = collect_post(&openapi_schema.paths);
+        populate_payload(&mut posts, openapi_schema.components.unwrap());
+        let f = posts.first().unwrap();
+        assert_ne!(f.payload, None);
+
+        let s = f.payload.clone();
+        let s = s.unwrap();
+        let mut props = property_for_schema(&s);
+
+        assert_ne!(props.len(), 0);
+
+        assert!(props.contains_key("email"));
+        assert!(props.contains_key("password"));
+        assert!(props.contains_key("org"));
+
+        let combs = create_combination_property(&mut props);
+        assert_eq!(combs.len(), 7);
+    }
+
+    #[test]
+    fn check_post_payload_full_example_obj() {
+        let s = std::include_str!("./testdata/post_login_obj_example.yml");
+        let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+        let mut posts = collect_post(&openapi_schema.paths);
+        populate_payload(&mut posts, openapi_schema.components.unwrap());
+        let f = posts.first().unwrap();
+        assert_ne!(f.payload, None);
+
+        let s = f.payload.clone();
+        let s = s.unwrap();
+        let mut props = property_for_schema(&s);
+
+        assert_ne!(props.len(), 0);
+
+        assert!(props.contains_key("email"));
+        assert!(props.contains_key("password"));
+        assert!(props.contains_key("org"));
+
+        let combs: Vec<Vec<(&String, &PropertyField)>> = create_combination_property(&mut props);
+        assert_eq!(combs.len(), 7);
+    }
+
+    #[test]
+    fn check_post_payload_single_example_properties() {
+        let s = std::include_str!("./testdata/post_login_properties_example.yml");
+        let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+        let mut posts = collect_post(&openapi_schema.paths);
+        populate_payload(&mut posts, openapi_schema.components.unwrap());
+        let f = posts.first().unwrap();
+        assert_ne!(f.payload, None);
+
+        let s = f.payload.clone();
+        let s = s.unwrap();
+        let mut props = property_for_schema(&s);
+
+        assert_ne!(props.len(), 0);
+
+        assert!(props.contains_key("email"));
+        assert!(props.contains_key("password"));
+        assert!(props.contains_key("org"));
+
+        let combs: Vec<Vec<(&String, &PropertyField)>> = create_combination_property(&mut props);
+        assert_eq!(combs.len(), 7);
+    }
+
+    #[test]
+    fn skip_non_json_content() {
+        let s = std::include_str!("./testdata/post_non_json_content.yml");
+        let openapi_schema = serde_yaml::from_str(s);
+        assert!(openapi_schema.is_ok());
+
+        let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+        let posts = collect_post(&openapi_schema.paths);
+
+        assert_eq!(posts.len(), 0);
+    }
+
+    #[test]
+    fn find_jwt_token_in_components() {
+        let s = std::include_str!("./testdata/get_more_info_with_jwt.yml");
+        let openapi_schema = serde_yaml::from_str(s);
+        assert!(openapi_schema.is_ok());
+
+        let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+        let components = openapi_schema.components.unwrap();
+        let jwt = get_jwt_token(&components);
+
+        assert!(jwt.is_some());
+        assert_eq!(jwt.unwrap(), "bearerAuth");
+    }
+
+    #[test]
+    fn skip_deprecated() {
+        {
+            let s = std::include_str!("./testdata/get_info_deprecated.yml");
+            let openapi_schema = serde_yaml::from_str(s);
+            assert!(openapi_schema.is_ok());
+
+            let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+            let gets = collect_gets(&openapi_schema.paths);
+            assert_eq!(gets.len(), 0);
+        }
+        {
+            let s = std::include_str!("./testdata/post_login_deprecated.yml");
+            let openapi_schema = serde_yaml::from_str(s);
+            assert!(openapi_schema.is_ok());
+
+            let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+            let posts = collect_post(&openapi_schema.paths);
+            assert_eq!(posts.len(), 0);
+        }
+    }
+}
---
+++ tests/integrations.rs
@@ -0,0 +1,164 @@
+use axum::RequestPartsExt;
+
+#[derive(Clone)]
+struct AppState {}
+
+async fn run_api() -> String {
+    let state = std::sync::Arc::new(AppState {});
+    let app = axum::Router::new()
+        .route("/api/v1/org/info", axum::routing::get(info))
+        .route("/api/v1/org/more/info", axum::routing::get(more_info))
+        .route("/api/v1/org/login", axum::routing::post(login_handler))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await.unwrap();
+
+    let base_url = format!("http://{}", listener.local_addr().unwrap());
+    tracing::info!("listening on {}", base_url);
+
+    tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    base_url
+}
+
+/// LoginRequest is the body expected for a simple login request.
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+struct LoginRequest {
+    email: String,
+    password: String,
+    org: String,
+}
+
+async fn login_handler(
+    axum::extract::State(_): axum::extract::State<std::sync::Arc<AppState>>,
+    axum::Json(payload): axum::Json<LoginRequest>,
+) -> axum::Json<String> {
+    tracing::info!("Received login request: {:?}", payload);
+    axum::Json(format!(
+        "User {} from org {} is trying to login",
+        payload.email, payload.org
+    ))
+}
+
+pub enum AuthError {
+    TokenNotFound,
+}
+
+// This allows to be used as the `Rejection` type in the `FromRequestPars` trait.
+impl axum::response::IntoResponse for AuthError {
+    fn into_response(self) -> axum::response::Response {
+        use axum::http::StatusCode;
+
+        let (status, error_message) = match self {
+            Self::TokenNotFound => (StatusCode::UNAUTHORIZED, "Token not found"),
+        };
+        let body = axum::Json(serde_json::json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
+
+#[async_trait::async_trait]
+impl<S> axum::extract::FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(
+        parts: &mut axum::http::request::Parts,
+        _state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        use axum_extra::headers::authorization::Bearer;
+        use axum_extra::headers::Authorization;
+
+        let axum_extra::TypedHeader(Authorization(bearer)) = parts
+            .extract::<axum_extra::TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::TokenNotFound)?;
+
+        tracing::info!("header bearer: {:?}", bearer);
+
+        let token_data = Claims {
+            token_received: bearer.token().to_owned(),
+        };
+
+        Ok(token_data)
+    }
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct Claims {
+    token_received: String,
+}
+
+async fn info() -> axum::Json<&'static str> {
+    axum::Json("Hello, World!")
+}
+
+// this return the token populated during the request, this way we can use it for test checks.
+async fn more_info(claims: Claims) -> axum::Json<String> {
+    axum::Json(claims.token_received)
+}
+
+/// This is a fake test to make sure the test suite is setup with tracing.
+#[test]
+fn fake_test() {
+    tracing_subscriber::fmt::init();
+    assert!(true);
+}
+
+#[tokio::test]
+async fn get_info_simple() {
+    let url = run_api().await;
+
+    let s = std::include_str!("../src/testdata/get_info.yml");
+    let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+    let r = fiuto::do_it(openapi_schema, Some(url), None).await;
+
+    assert!(r.is_ok());
+    let r = r.unwrap();
+
+    assert_eq!(r.len(), 1);
+
+    // TODO: add more checks about code returned/expected
+}
+
+#[tokio::test]
+async fn post_login() {
+    let url = run_api().await;
+
+    let s = std::include_str!("../src/testdata/post_login.yml");
+    let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+    let r = fiuto::do_it(openapi_schema, Some(url), None).await;
+
+    assert!(r.is_ok());
+    let r = r.unwrap();
+
+    assert_eq!(r.len(), 1); // 1 endpoint
+
+    let combinations = r.get(0).unwrap();
+    assert_eq!(combinations.len(), 7); // 7 combinations
+
+    // TODO: add more checks about code returned/expected
+}
+
+#[tokio::test]
+async fn get_with_jwt() {
+    let url = run_api().await;
+
+    let s = std::include_str!("../src/testdata/get_more_info_with_jwt.yml");
+    let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+    let token = Some("test_token".to_owned());
+    let r = fiuto::do_it(openapi_schema, Some(url), token).await;
+
+    assert!(r.is_ok());
+
+    let r = r.unwrap();
+    assert_eq!(r.len(), 1);
+
+    // TODO: Add more check, specially about the token, but we need responses more info, not just code.
+}
