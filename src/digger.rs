10fff90811a59da8612f19040aced6fdee68c4cd
Federico Guerinoni <guerinoni.federico@gmail.com>
2025-01-13 15:36:40 +0100

    Implement a more sophisticated shuffling for property
    This supports in a good way also nested property.

---
+++ src/digger.rs
@@ -0,0 +1,292 @@
+/// Node is a struct that represents a single property in JSON data.
+/// So this has a name and a possible value or a list of children, for example when you
+/// have a nested object.
+#[derive(Debug)]
+pub struct Node {
+    pub name: String,
+    pub value: serde_json::Value,
+    pub parent: Option<std::rc::Weak<std::cell::RefCell<Node>>>,
+    pub children: Vec<std::rc::Rc<std::cell::RefCell<Node>>>,
+}
+
+impl Node {
+    pub fn new(name: &str, value: serde_json::Value) -> std::rc::Rc<std::cell::RefCell<Self>> {
+        std::rc::Rc::new(std::cell::RefCell::new(Node {
+            name: name.to_string(),
+            value,
+            parent: None,
+            children: Vec::new(),
+        }))
+    }
+
+    /// Add a child node to the current node.
+    fn add_child(
+        parent: &std::rc::Rc<std::cell::RefCell<Node>>,
+        child: std::rc::Rc<std::cell::RefCell<Node>>,
+    ) {
+        child.borrow_mut().parent = Some(std::rc::Rc::downgrade(parent));
+        parent.borrow_mut().children.push(child);
+    }
+}
+
+/// Diggere is a struct that holds the current state while populating the tree going through
+/// the JSON data of all components.
+pub struct Digger {
+    components: openapiv3::Components,
+    pub root: std::rc::Rc<std::cell::RefCell<Node>>,
+    current: std::rc::Rc<std::cell::RefCell<Node>>,
+}
+
+impl Digger {
+    pub fn new(components: openapiv3::Components) -> Self {
+        let root = Node::new("root", serde_json::Value::Null);
+
+        Digger {
+            components,
+            root: std::rc::Rc::clone(&root),
+            current: root,
+        }
+    }
+
+    fn add_child_and_enter(&mut self, child_name: &str) {
+        let child = Node::new(child_name, serde_json::Value::Null);
+        Node::add_child(&self.current, std::rc::Rc::clone(&child));
+        self.current = child;
+    }
+
+    fn exit_one_level(&mut self) {
+        let parent = self
+            .current
+            .borrow()
+            .parent
+            .as_ref()
+            .unwrap()
+            .upgrade()
+            .unwrap();
+        self.current = parent;
+    }
+
+    pub fn dig(&mut self, schema: openapiv3::Schema) -> Result<(), String> {
+        let t = match schema.schema_kind {
+            openapiv3::SchemaKind::Type(t) => t,
+            _ => {
+                tracing::warn!("schema kind not handled");
+                return Err("schema kind not handled".to_owned());
+            }
+        };
+
+        let obj = match t {
+            openapiv3::Type::Object(o) => o,
+            _ => {
+                tracing::warn!("Unsupported type: {:?}", t);
+                return Err("Unsupported type".to_owned());
+            }
+        };
+
+        for (name, p) in obj.properties {
+            match p {
+                openapiv3::ReferenceOr::Item(item) => {
+                    let v = match item.schema_data.example {
+                        Some(e) => e,
+                        None => {
+                            tracing::warn!("No example found in the schema data {}", name);
+                            continue;
+                        }
+                    };
+
+                    let n = Node::new(&name, v);
+                    n.borrow_mut().parent = Some(std::rc::Rc::downgrade(&self.current));
+
+                    self.current.borrow_mut().children.push(n);
+                }
+                openapiv3::ReferenceOr::Reference { reference } => {
+                    let (_, schema) = reference_to_schema_and_name(reference, &self.components)?;
+
+                    self.add_child_and_enter(&name);
+
+                    self.dig(schema)?;
+
+                    self.exit_one_level();
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// It converts a reference from full name to a tuple with schema name and schema.
+fn reference_to_schema_and_name(
+    reference: String,
+    components: &openapiv3::Components,
+) -> Result<(String, openapiv3::Schema), String> {
+    let name = reference.trim_start_matches("#/components/schemas/");
+    let schema = match components.schemas.get(name) {
+        Some(s) => s,
+        None => {
+            let msg = format!("No schema found for reference: {}", reference);
+            return Err(msg);
+        }
+    };
+
+    let s = schema.as_item();
+    let s = s.unwrap();
+
+    Ok((name.to_owned(), s.clone()))
+}
+
+#[cfg(test)]
+pub fn load_flat_level() -> std::rc::Rc<std::cell::RefCell<Node>> {
+    let s = std::include_str!("./testdata/post_login.yml");
+    let openapi_schema = serde_yaml::from_str(&s);
+    let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+    let components = openapi_schema.components.unwrap();
+    let posts = crate::collector::collect_post(&openapi_schema.paths, &components);
+
+    let mut digger = Digger::new(components);
+    let f = posts.first().unwrap();
+    let s = f.payload.clone();
+    let s = s.unwrap();
+
+    let result = digger.dig(s);
+    assert!(result.is_ok());
+
+    digger.root
+}
+
+#[cfg(test)]
+pub fn load_nested() -> std::rc::Rc<std::cell::RefCell<Node>> {
+    let s = std::include_str!("./testdata/post_info_nested_property.yml");
+    let openapi_schema = serde_yaml::from_str(&s);
+    let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+    let components = openapi_schema.components.unwrap();
+    let posts = crate::collector::collect_post(&openapi_schema.paths, &components);
+
+    let mut digger = Digger::new(components);
+    let f = posts.first().unwrap();
+    let s = f.payload.clone();
+    let s = s.unwrap();
+
+    let result = digger.dig(s);
+    assert!(result.is_ok());
+
+    digger.root
+}
+
+#[cfg(test)]
+pub fn load_nested_2() -> std::rc::Rc<std::cell::RefCell<Node>> {
+    let s = std::include_str!("./testdata/post_info_nested_property_2.yml");
+    let openapi_schema = serde_yaml::from_str(&s);
+    let openapi_schema: openapiv3::OpenAPI = openapi_schema.unwrap();
+    let components = openapi_schema.components.unwrap();
+    let posts = crate::collector::collect_post(&openapi_schema.paths, &components);
+
+    let mut digger = Digger::new(components);
+    let f = posts.first().unwrap();
+    let s = f.payload.clone();
+    let s = s.unwrap();
+
+    let result = digger.dig(s);
+    assert!(result.is_ok());
+
+    digger.root
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn nested() {
+        let s = std::include_str!("./testdata/post_info_nested_property.yml");
+        let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+        let components = openapi_schema.components.unwrap();
+        let posts = crate::collector::collect_post(&openapi_schema.paths, &components);
+
+        let f = posts.first().unwrap();
+        assert_ne!(f.payload, None);
+
+        let s = f.payload.clone();
+        let s = s.unwrap();
+
+        let mut digger = Digger::new(components);
+        let result = digger.dig(s);
+        assert!(result.is_ok());
+
+        // check the tree generated from the schema
+        let root = digger.root.borrow();
+        assert_eq!(root.children.len(), 1);
+
+        let child = root.children.first().unwrap();
+        assert_eq!(child.borrow().name, "hq");
+
+        let hq = child.borrow();
+        assert_eq!(hq.children.len(), 5);
+
+        let mut children = hq.children.iter();
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "address");
+
+        let hq = children.next().unwrap();
+        assert_eq!(hq.borrow().name, "postal_code");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "city");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "state_region");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "country");
+
+        assert!(children.next().is_none());
+    }
+
+    #[test]
+    fn nested_with_simple_along() {
+        let s = std::include_str!("./testdata/post_info_nested_property_2.yml");
+        let openapi_schema: openapiv3::OpenAPI = serde_yaml::from_str(s).unwrap();
+        let components = openapi_schema.components.unwrap();
+        let posts = crate::collector::collect_post(&openapi_schema.paths, &components);
+
+        let f = posts.first().unwrap();
+        assert_ne!(f.payload, None);
+
+        let s = f.payload.clone();
+        let s = s.unwrap();
+
+        let mut digger = Digger::new(components);
+        let result = digger.dig(s);
+        assert!(result.is_ok());
+
+        let root = digger.root.borrow();
+        assert_eq!(root.children.len(), 2);
+
+        let hq = root.children.first().unwrap();
+        assert_eq!(hq.borrow().name, "hq");
+
+        let plan_property = root.children.last().unwrap();
+        assert_eq!(plan_property.borrow().name, "other");
+
+        let hq = hq.borrow();
+        assert_eq!(hq.children.len(), 5);
+
+        let mut children = hq.children.iter();
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "address");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "postal_code");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "city");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "state_region");
+
+        let child = children.next().unwrap();
+        assert_eq!(child.borrow().name, "country");
+
+        assert!(children.next().is_none());
+    }
+}
---
+++ src/shuffler.rs
@@ -0,0 +1,313 @@
+
+pub fn do_it(
+    point: std::rc::Rc<std::cell::RefCell<crate::digger::Node>>,
+) -> Vec<std::collections::HashMap<String, serde_json::Value>> {
+    let mut properties = vec![];
+
+    let mut sub_properties = std::collections::HashMap::new();
+
+    // this check if all kids are leaves, becuse if only one has children, we need to go deeper.
+    // otherwise, we can generate the combinations.
+    for c in &point.borrow().children {
+        let n = c.borrow();
+        properties.push((n.name.clone(), n.value.clone()));
+
+        if !n.children.is_empty() {
+            let m = do_it(std::rc::Rc::clone(c));
+            sub_properties.insert(c.borrow().name.clone(), m);
+            continue;
+        }
+    }
+
+    let mut combs = vec![];
+
+    let total_combinations = (1 << properties.len()) - 1;
+    for mask in 1..=total_combinations {
+        let mut c = std::collections::HashMap::new();
+        for (i, p) in properties.iter().enumerate() {
+            if (mask & (1 << i)) == 0 {
+                continue;
+            }
+
+            c.insert(p.0.clone(), p.1.clone());
+        }
+
+        combs.push(c);
+    }
+
+    let mut to_push = vec![];
+
+    // for every combination, we need to add the sub properties to it.
+    // if you have for example:
+    // - string
+    // - object
+    // you will have
+    // - [string]
+    // - [null]
+    // - [object (with all possible combinations)]
+    // [string, object (with all possible combinations)]
+    for c in &combs {
+        for (k, v) in &sub_properties {
+            v.iter()
+                .map(|variant| serde_json::to_value(variant).unwrap())
+                .for_each(|vv| {
+                    let mut h = c.clone();
+                    h.insert(k.clone(), vv.clone());
+                    to_push.push(h);
+                });
+        }
+    }
+
+    for t in to_push {
+        combs.push(t);
+    }
+
+    combs
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn one_level_properties() {
+        // here we have:
+        // - email
+        // - org
+        // - password
+        let root = crate::digger::load_flat_level();
+        let c = crate::shuffler::do_it(root);
+
+        println!("{:#?}", c);
+
+        assert_eq!(c.len(), 7);
+
+        let zero = c.get(0).unwrap();
+        assert!(zero.contains_key("email"));
+
+        let one = c.get(1).unwrap();
+        assert!(one.contains_key("org"));
+
+        let two = c.get(2).unwrap();
+        assert!(two.contains_key("email"));
+        assert!(two.contains_key("org"));
+
+        let three = c.get(3).unwrap();
+        assert!(three.contains_key("password"));
+
+        let four = c.get(4).unwrap();
+        assert!(four.contains_key("email"));
+        assert!(four.contains_key("password"));
+
+        let five = c.get(5).unwrap();
+        assert!(five.contains_key("org"));
+        assert!(five.contains_key("password"));
+
+        let six = c.get(6).unwrap();
+        assert!(six.contains_key("email"));
+        assert!(six.contains_key("org"));
+        assert!(six.contains_key("password"));
+    }
+
+    #[test]
+    fn one_as_object() {
+        // here we have:
+        // - hq -> address, postal_code, city, state_region, country
+
+        let root = crate::digger::load_nested();
+        let c = crate::shuffler::do_it(root);
+
+        println!("{:#?}", c);
+
+        assert_eq!(c.len(), 32);
+
+        let zero = c.get(0).unwrap();
+        assert!(zero.contains_key("hq"));
+        assert_eq!(zero.get("hq").unwrap(), &serde_json::Value::Null);
+
+        let one = c.get(1).unwrap();
+        let hq = one.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+
+        let two = c.get(2).unwrap();
+        let hq = two.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+
+        let three = c.get(3).unwrap();
+        let hq = three.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+
+        let four = c.get(4).unwrap();
+        let hq = four.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("city"));
+
+        let five = c.get(5).unwrap();
+        let hq = five.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("city"));
+
+        let six = c.get(6).unwrap();
+        let hq = six.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+
+        let seven = c.get(7).unwrap();
+        let hq = seven.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+
+        let eight = c.get(8).unwrap();
+        let hq = eight.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("state_region"));
+
+        let nine = c.get(9).unwrap();
+        let hq = nine.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("state_region"));
+
+        let ten = c.get(10).unwrap();
+        let hq = ten.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("state_region"));
+
+        let eleven = c.get(11).unwrap();
+        let hq = eleven.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("state_region"));
+
+        let twelve = c.get(12).unwrap();
+        let hq = twelve.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+
+        let thirteen = c.get(13).unwrap();
+        let hq = thirteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+
+        let fourteen = c.get(14).unwrap();
+        let hq = fourteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+
+        let fifteen = c.get(15).unwrap();
+        let hq = fifteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+
+        let sixteen = c.get(16).unwrap();
+        let hq = sixteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("country"));
+
+        let seventeen = c.get(17).unwrap();
+        let hq = seventeen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("country"));
+
+        let eighteen = c.get(18).unwrap();
+        let hq = eighteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("country"));
+
+        let nineteen = c.get(19).unwrap();
+        let hq = nineteen.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("country"));
+
+        let twenty = c.get(20).unwrap();
+        let hq = twenty.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_one = c.get(21).unwrap();
+        let hq = twenty_one.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_two = c.get(22).unwrap();
+        let hq = twenty_two.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_three = c.get(23).unwrap();
+        let hq = twenty_three.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_four = c.get(24).unwrap();
+        let hq = twenty_four.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_five = c.get(25).unwrap();
+        let hq = twenty_five.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_six = c.get(26).unwrap();
+        let hq = twenty_six.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_seven = c.get(27).unwrap();
+        let hq = twenty_seven.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_eight = c.get(28).unwrap();
+        let hq = twenty_eight.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let twenty_nine = c.get(29).unwrap();
+        let hq = twenty_nine.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let thirty = c.get(30).unwrap();
+        let hq = thirty.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+
+        let thirty_one = c.get(31).unwrap();
+        let hq = thirty_one.get("hq").unwrap().as_object().unwrap();
+        assert!(hq.contains_key("address"));
+        assert!(hq.contains_key("postal_code"));
+        assert!(hq.contains_key("city"));
+        assert!(hq.contains_key("state_region"));
+        assert!(hq.contains_key("country"));
+    }
+
+    #[test]
+    fn one_string_and_one_as_object() {
+        // here we have:
+        // - other (string)
+        // - hq -> address, postal_code, city, state_region, country
+
+        let root = crate::digger::load_nested_2();
+        let c = crate::shuffler::do_it(root);
+
+        println!("{:#?} - {}", c, c.len());
+
+        assert_eq!(c.len(), 96);
+    }
+}
